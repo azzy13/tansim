76867cf05cc149dbc38079ccd142040f4d57784a
azzy13 <azharhasan013@gmail.com>
2020-11-08 21:26:57 +0530

    nav

--- js/style.js
+++ js/style.js
@@ -1,5 +1,26 @@
 // For card shadow
 $(document).ready(function () {
+  $('.mobileMenu>div>div>button>i').on('click', function () {
+    $('.mobileMenu').collapse('hide');
+  });
+
+  var fixHeight = function () {
+    $('.navbar-nav').css(
+      'max-height',
+      document.documentElement.clientHeight - 150,
+    );
+  };
+  fixHeight();
+  $(window).resize(function () {
+    fixHeight();
+  });
+  $('.navbar .navbar-toggler').on('click', function () {
+    fixHeight();
+  });
+  $('.navbar-toggler, .overlay').on('click', function () {
+    $('.mobileMenu, .overlay').toggleClass('open');
+  });
+
   $('#ideation').click();
   $('.card-hover').hover(
     function () {
